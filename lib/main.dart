63c53a07555c3be447ed753c0510c02cac5f2e80
Leonardo Freitas <leonardofreitasdev@gmail.com>
2020-05-13 23:03:53 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'utils/app-routes.dart';
+import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'Roboto'
+          )
+        )
+      ),
+      routes: {
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+      },
+    );
+  }
+}
